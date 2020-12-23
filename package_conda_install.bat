21b4f912a3220c4b4ca2c45a47a07f29eb2d4a71
Nonki <jun.hamada2@gmail.com>
2020-12-23 21:49:47 +0900

    AnacondaやMinicondaを使用する場合のパッケージの一覧出力用バッチファイルを追加

---
+++ package_conda_install.bat
@@ -0,0 +1,2 @@
+@rem PythonインタプリタとしてAnaconda（miniconda）を使用する場合の環境構築用バッチファイル
+conda env create --file myenv.yaml
---
+++ package_conda_list-export.bat
@@ -0,0 +1,2 @@
+@rem PythonインタプリタとしてAnaconda（miniconda）を使用する場合のパッケージの一覧を出力するバッチファイル
+conda env export > myenv.yaml
